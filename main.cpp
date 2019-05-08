789a5b6c70d1f809fad7b015a60067dc9b9742f4
Svyat <sstndart@gmail.com>
2019-05-08 23:48:48 +0300

    Animation example

--- main.cpp
+++ main.cpp
@@ -2,24 +2,44 @@
 #include <iostream>
 
 #include "Cell.h"
+#include "AnimatedSprite.h"
 
 using namespace sf;
 using namespace std;
 
 int main()
 {
-    int a;
-    RenderWindow window (VideoMode(500, 500), "GBI");
+    // setup window
+    Vector2i screenDimensions(800,600);
+    RenderWindow window(VideoMode(screenDimensions.x, screenDimensions.y), "Animation");
+    window.setFramerateLimit(60);
 
-    Image Qwar2;
-    Qwar2.loadFromFile("Images/Quar.png");
+    // load texture
+    Texture texture;
+    if (!texture.loadFromFile("Images/tex.png"))
+    {
+        cout << "Failed to load texture\n";
+        return 1;
+    }
 
-    Texture Qwar0;
-    Qwar0.loadFromImage(Qwar2);
+    Animation loading;
+    loading.setSpriteSheet(texture);
+    loading.addFrame(IntRect(0, 0, 256, 256));
+    loading.addFrame(IntRect(256, 0, 256, 256));
+    loading.addFrame(IntRect(512, 0, 256, 256));
+    loading.addFrame(IntRect(768, 0, 256, 256));
+    loading.addFrame(IntRect(0, 256, 256, 256));
+    loading.addFrame(IntRect(256, 256, 256, 256));
+    loading.addFrame(IntRect(512, 256, 256, 256));
+    loading.addFrame(IntRect(768, 256, 256, 256));
 
-    Sprite Qwar;
-    Qwar.setTexture(Qwar0);
-    Qwar.setPosition(0, 0);
+    Animation* currentAnimation = &loading;
+
+    AnimatedSprite animatedSprite(seconds(0.2), true, false);
+    animatedSprite.setPosition(Vector2f(screenDimensions / 4));
+
+    Clock frameClock;
+
 
     while (window.isOpen())
     {
@@ -29,11 +49,21 @@ int main()
             if (event.type == Event::Closed){
                 window.close();
             }
+            if (event.type == Event::KeyPressed && event.key.code == Keyboard::Escape)
+                window.close();
         }
 
+        Time frameTime = frameClock.restart();
+        animatedSprite.play(*currentAnimation);
+
+        // update AnimatedSprite
+        animatedSprite.update(frameTime);
+
+        // draw
         window.clear();
-        window.draw(Qwar);
+        window.draw(animatedSprite);
         window.display();
     }
+
     return 0;
 }
